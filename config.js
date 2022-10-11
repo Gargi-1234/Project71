e466fdd10c72275d0daafb0ad5f2031489ee671f
Gargi-1234 <87595595+Gargi-1234@users.noreply.github.com>
2022-10-11 09:31:25 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+//Paste your firebaseConfig here
+const firebaseConfig = {
+    apiKey: "AIzaSyCIX8QqfsQvlhP0HceJmOqpK4z-Z1LXv1g",
+    authDomain: "project71-c29d9.firebaseapp.com",
+    projectId: "project71-c29d9",
+    storageBucket: "project71-c29d9.appspot.com",
+    messagingSenderId: "553435488812",
+    appId: "1:553435488812:web:9b798bb3af7b816e496789"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
